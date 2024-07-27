cc51344ddd562f5f80f475952b0dbee0069e60e3
BISWAJEET SAHOO <56734700+Biswajeet-23@users.noreply.github.com>
2024-07-27 21:21:13 +0530

    Update index.js

--- blog_app/api/index.js
+++ blog_app/api/index.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 //middlewares
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "https://mern-blog-app-frontend-wkow.onrender.com" }));
 //LOCAL_HOST
 // app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
 app.use(express.json());
